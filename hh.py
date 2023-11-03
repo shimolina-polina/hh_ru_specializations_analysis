243289c96d1e66fffacc25fba9c8e19ded00450f
polina-shimolina <shimolinapk@gmail.com>
2023-11-03 16:37:03 +0300

    Удалены дубликаты диаграмм

--- hh.py
+++ hh.py
@@ -2,7 +2,7 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 import re
-
+import numpy as np
 
 
 def main():
@@ -100,45 +100,7 @@ def main():
     fig_experience_counts = px.bar(filtered_df.groupby('Опыт работы')['id'].count().reset_index(), x='Опыт работы', y='id', title='Распределение вакансий по опыту работы')
     st.plotly_chart(fig_experience_counts)
 
-    skills_counts = {}
-
-    for skill in unique_skills:
-        skill = re.escape(skill)
-        skills_counts[skill] = filtered_df['Ключевые навыки'].str.count(skill).sum()
-        
-    sorted_skills_counts = dict(sorted(skills_counts.items(), key=lambda x: x[1], reverse=True))
-
-    top_10_df = pd.DataFrame(list(sorted_skills_counts.items())[:10])
-    top_10_df.columns = ['Навык', 'Количество']
-
-    fig = px.bar(top_10_df, x='Навык', y='Количество', title='Топ-10 навыков')
-    st.plotly_chart(fig, use_container_width=True)
-
-    # График количество вакансий в зависимости от зарплаты для разных стажей
-
-    df = filtered_df.groupby(['Опыт работы', 'Зарплата от']).count()['id'].reset_index()
-
-    fig_salary_from_experience = px.bar(df, x='Зарплата от', y='id', color='Опыт работы', 
-                barmode='group', title='Распределение нижних границ зарплат')
-
-    fig_salary_from_experience.update_layout(xaxis_title='Зарплата от, тыс. руб.',
-                    yaxis_title='Количество вакансий')
-
-    st.plotly_chart(fig_salary_from_experience)
-
-    df = filtered_df.groupby(['Опыт работы', 'Зарплата до']).count()['id'].reset_index()
-
-    fig_salary_to_experience = px.bar(df, x='Зарплата до', y='id', color='Опыт работы', 
-                barmode='group', title='Распределение верхних границ зарплат')
-
-    fig_salary_to_experience.update_layout(xaxis_title='Зарплата до, тыс. руб.',
-                    yaxis_title='Количество вакансий')
-    st.plotly_chart(fig_salary_to_experience)
-
-    # График количество вакансий в зависимости от опыта
-    fig_experience_counts = px.bar(filtered_df.groupby('Опыт работы')['id'].count().reset_index(), x='Опыт работы', y='id', title='Распределение вакансий по опыту работы')
-    st.plotly_chart(fig_experience_counts)
-
+    # Диаграмма топ-10 навыков для специальности
     skills_counts = {}
 
     for skill in unique_skills:
